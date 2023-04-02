4c0e9653e6893aa3877ded4dafbd573c238d3cd6
Andy Tsang <16506503+aydtsang@users.noreply.github.com>
2023-04-02 18:08:34 -0400

    fix indentation

--- table-project/src/components/UsersTable.tsx
+++ table-project/src/components/UsersTable.tsx
@@ -4,21 +4,21 @@ export default async function UsersTable() {
     const users = await fetchUsers();
     return (
         <table>
-        <thead>
-            <tr>
-            <th>User ID</th>
-            <th>Name</th>
-            <th>Website</th>
-            </tr>
-        </thead>
-        <tbody>
-            {users.map((user) => (
-            <tr key={user.id}>
-                <td>{user.name}</td>
-                <td>{user.website}</td>
-            </tr>
-            ))}
-        </tbody>
+            <thead>
+                <tr>
+                    <th>User ID</th>
+                    <th>Name</th>
+                    <th>Website</th>
+                </tr>
+            </thead>
+            <tbody>
+                {users.map((user) => (
+                <tr key={user.id}>
+                    <td>{user.name}</td>
+                    <td>{user.website}</td>
+                </tr>
+                ))}
+            </tbody>
         </table>
     );
 }
