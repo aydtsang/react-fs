c926f8acf88dee0ec2f0f6bda4d8d0da8c9590e6
Andy Tsang <16506503+aydtsang@users.noreply.github.com>
2023-04-02 18:11:06 -0400

    notice the expected error for server components

--- table-project/src/app/page.tsx
+++ table-project/src/app/page.tsx
@@ -3,6 +3,7 @@ import UsersTable from "~/components/UsersTable";
 export default function Home() {
   return (
     <main>
+      {/* @ts-expect-error Server Component */}
       <UsersTable/>
     </main>
   )
